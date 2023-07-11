4401118f90ead9d1e7a80892189bcaaaf8ef5a00
Lejian Luo <luolejian@outlook.com>
2023-07-10 22:19:47 -0400

    enable vercel analytics

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux'
 import store from './store/store'
 import { BrowserRouter } from 'react-router-dom';
+import { inject } from '@vercel/analytics';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,3 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
+inject();
